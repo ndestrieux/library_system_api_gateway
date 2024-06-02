2625d50241a1b654f10527b6890c1cee31e7fd22
ndestrieux <n.destrieux@gmail.com>
2024-06-02 15:52:53 +0200

    Testing router for forum microservice, and adapting library router testing.

--- tests/test_gateway_router/test_library_router.py
+++ tests/test_gateway_router/test_library_router.py
@@ -7,14 +7,18 @@ from src.dependencies.gateway_routers import LibraryRouter
 from src.dependencies.graphql.request_body_builders import (
     AuthorMutationRequestBody,
     AuthorQueryRequestBody,
-    BookMutationRequestBody,
-    BookQueryRequestBody,
 )
 
 
-class MockRequestBody:
+class MockQueryRequestBody:
     def __str__(self):
         return "query {author { Dale Cooper }}"
+
+
+class MockMutationRequestBody:
+    def __str__(self):
+        return """mutation createAuthor {author(first_name: "Dale", last_name: "Cooper")
+        {id first_name middle_name last_name}}"""
 
 
 @pytest.mark.anyio
@@ -24,15 +28,15 @@ class MockRequestBody:
 )
 @patch(
     "tests.test_gateway_router.test_library_router.AuthorQueryRequestBody",
-    return_value=MockRequestBody,
+    return_value=MockQueryRequestBody,
 )
-async def test_library_author_query_router(
-    graphql_query_mocker, asyncclient_get_mocker
+async def test_library_router_with_get_method(
+    graphql_query_mocker, asyncclient_get_mocker, response_data
 ):
     author_data_obj = AuthorQueryRequestBody()
     router = LibraryRouter("/authors/", "get", "user_1234", author_data_obj)
     response = await router.send_request()
-    assert response.json() == {"data": "Some data", "errors": []}
+    assert response.json() == response_data
 
 
 @pytest.mark.anyio
@@ -42,46 +46,12 @@ async def test_library_author_query_router(
 )
 @patch(
     "tests.test_gateway_router.test_library_router.AuthorMutationRequestBody",
-    return_value=MockRequestBody,
+    return_value=MockMutationRequestBody,
 )
-async def test_library_author_mutation_router(
-    graphql_query_mocker, asyncclient_get_mocker
+async def test_library_router_with_post_method(
+    graphql_query_mocker, asyncclient_get_mocker, response_data
 ):
     author_data_obj = AuthorMutationRequestBody()
     router = LibraryRouter("/authors/", "post", "user_1234", author_data_obj)
     response = await router.send_request()
-    assert response.json() == {"data": "Some data", "errors": []}
-
-
-@pytest.mark.anyio
-@patch(
-    "src.dependencies.gateway_routers.httpx.AsyncClient.get",
-    return_value=httpx.Response(200, json={"data": "Some data", "errors": []}),
-)
-@patch(
-    "tests.test_gateway_router.test_library_router.BookQueryRequestBody",
-    return_value=MockRequestBody,
-)
-async def test_library_book_query_router(graphql_query_mocker, asyncclient_get_mocker):
-    author_data_obj = BookQueryRequestBody()
-    router = LibraryRouter("/books/", "get", "user_1234", author_data_obj)
-    response = await router.send_request()
-    assert response.json() == {"data": "Some data", "errors": []}
-
-
-@pytest.mark.anyio
-@patch(
-    "src.dependencies.gateway_routers.httpx.AsyncClient.post",
-    return_value=httpx.Response(200, json={"data": "Some data", "errors": []}),
-)
-@patch(
-    "tests.test_gateway_router.test_library_router.BookMutationRequestBody",
-    return_value=MockRequestBody,
-)
-async def test_library_book_mutation_router(
-    graphql_query_mocker, asyncclient_get_mocker
-):
-    author_data_obj = BookMutationRequestBody()
-    router = LibraryRouter("/authors/", "post", "user_1234", author_data_obj)
-    response = await router.send_request()
-    assert response.json() == {"data": "Some data", "errors": []}
+    assert response.json() == response_data
